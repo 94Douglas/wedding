210d72f3e965c787b4cbf54110c50e2dfb3dcb93
Douglas <94dousch@gafe.molndal.se>
2023-01-17 11:39:56 +0100

    Fixed info page.

--- client/src/pages/Information.js
+++ client/src/pages/Information.js
@@ -10,40 +10,44 @@ function Information() {
         {/* <div className="div-container"> */}
         <Header />
         {/* <div className="mb"></div> */}
-        <div className="card jc-center mb-5 width-fit">
-          <div className="card-body center">
-            <h5 className="card-title">När</h5>
-            <p className="card-text">
-              With supporting text below as a natural lead-in to additional
-              content.
-            </p>
+        <div className="f-left padding-left">
+          <div className="card jc-center mb-5 width-fit">
+            <div className="card-body center">
+              <h5 className="card-title">När</h5>
+              <p className="card-text">
+                With supporting text below as a natural lead-in to additional
+                content.
+              </p>
+            </div>
+          </div>
+          <div className="card jc-center mb-5 width-fit">
+            <div className="card-body center">
+              <h5 className="card-title">Var</h5>
+              <p className="card-text">
+                With supporting text below as a natural lead-in to additional
+                content.
+              </p>
+            </div>
           </div>
         </div>
-        <div className="card jc-center mb-5 width-fit">
-          <div className="card-body center">
-            <h5 className="card-title">Var</h5>
-            <p className="card-text">
-              With supporting text below as a natural lead-in to additional
-              content.
-            </p>
+        <div className="f-right padding-right">
+          <div className="card jc-center mb-5 width-fit">
+            <div className="card-body center">
+              <h5 className="card-title">Hur</h5>
+              <p className="card-text">
+                With supporting text below as a natural lead-in to additional
+                content.
+              </p>
+            </div>
           </div>
-        </div>
-        <div className="card jc-center mb-5 width-fit">
-          <div className="card-body center">
-            <h5 className="card-title">Hur</h5>
-            <p className="card-text">
-              With supporting text below as a natural lead-in to additional
-              content.
-            </p>
-          </div>
-        </div>
-        <div className="card jc-center mb-5 width-fit">
-          <div className="card-body center">
-            <h5 className="card-title ">Klädsel</h5>
-            <p className="card-text">
-              With supporting text below as a natural lead-in to additional
-              content.
-            </p>
+          <div className="card jc-center mb-5 width-fit">
+            <div className="card-body center">
+              <h5 className="card-title ">Klädsel</h5>
+              <p className="card-text">
+                With supporting text below as a natural lead-in to additional
+                content.
+              </p>
+            </div>
           </div>
         </div>
       </div>
